6f56986fb5b8ce961e188f3b82ce72baec13476b
Subramanian M <karthik10805@gmail.com>
2026-08-21 10:08:38 +0530

    Time: 0 ms (100%), Space: 12.5 MB (0.6%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def heightChecker(self, heights):
+        expected = sorted(heights)
+        count = 0
+        for i in range(len(heights)):
+            if heights[i] != expected[i]:
+                count += 1
+        return count
+        """
+        :type heights: List[int]
+        :rtype: int
+        """
+        """
+        :type heights: List[int]
+        :rtype: int
+        """
+        
