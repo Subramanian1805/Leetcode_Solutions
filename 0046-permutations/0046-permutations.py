f3d826f444a3c0b581fb260e14dab110bcee7db4
Subramanian M <karthik10805@gmail.com>
2026-08-29 19:19:50 +0530

    Time: 3 ms (54.49%), Space: 12.5 MB (57.44%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def permute(self, nums):
+        result = []
+        path = []
+        used = [False] * len(nums)
+
+        def backtrack():
+
+            if len(path) == len(nums):
+                result.append(path[:])
+                return
+
+            for i in range(len(nums)):
+
+                if used[i]:
+                    continue
+
+                used[i] = True
+                path.append(nums[i])
+
+                backtrack()
+
+                path.pop()
+                used[i] = False
+
+        backtrack()
+
+        return result
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
