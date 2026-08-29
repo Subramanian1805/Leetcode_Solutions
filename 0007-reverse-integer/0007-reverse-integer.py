36fe7b935e65178c59306e240260d73a3857d328
Subramanian M <karthik10805@gmail.com>
2026-08-29 20:58:15 +0530

    Update 0007-reverse-integer.py

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,7 +1,18 @@
 class Solution(object):
-    def reverse(self, x):
+    def reverse(self,x):
         """
         :type x: int
         :rtype: int
         """
-        
+
+        INT_MIN, INT_MAX = -2**31, 2**31 - 1
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+        rev = 0
+        while x != 0:
+            digit = x % 10
+            x //= 10
+            if rev > (INT_MAX - digit) // 10:
+                return 0
+            rev = rev * 10 + digit
+        return rev * sign
