6a956c9999555fddb01d2115d4bec46d3bc1d13d
Subramanian M <karthik10805@gmail.com>
2026-08-29 21:01:24 +0530

    Update 0283-move-zeroes.py

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -5,3 +5,10 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums in-place instead.
         """
         
+        org = nums
+
+        for i in org:
+            if i == 0:
+                org.remove(0)
+                org.append(0)
+        return list
